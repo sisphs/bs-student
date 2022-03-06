90e0ae9257f4e60c5441321a72b21cf9f2e1d915
‘黄涛’ <1400313388@qq.com>
2022-03-06 17:05:23 +0800

    fix: 答题界面修改

--- src/views/exam/paper/do.vue
+++ src/views/exam/paper/do.vue
@@ -1,51 +1,51 @@
 <template>
-  <div>
-    <el-row  class="do-exam-title">
-      <el-col :span="24">
+  <div class="my-do-exam">
+      <div class="my-do-exam-info">
+        <h3>{{form.name}}</h3>
+        <div class="my-do-exam-info-sore">
+          <span style="margin-right: 18px">总分：{{form.score}}</span>
+          <span>时长：{{form.suggestTime}}分钟</span>
+        </div>
+        <el-divider></el-divider>
         <span :key="item.itemOrder"  v-for="item in answer.answerItems">
              <el-tag :type="questionCompleted(item.completed)" class="do-exam-title-tag" @click="goAnchor('#question-'+item.itemOrder)">{{item.itemOrder}}</el-tag>
         </span>
-        <span class="do-exam-time">
-          <label>剩余时间：</label>
-          <label>{{formatSeconds(remainTime)}}</label>
-        </span>
-      </el-col>
-    </el-row>
-    <el-row  class="do-exam-title-hidden">
-      <el-col :span="24">
+        <el-divider></el-divider>
+        <div class="my-do-exam-info-time">
+          <label>剩余时间</label><br>
+          <label style="font-size: 18px; color: red;">{{formatSeconds(remainTime)}}</label>
+        </div>
+        <el-divider></el-divider>
+        <el-row class="do-align-center">
+             <el-button type="primary" @click="submitForm">提交试卷</el-button>
+           </el-row>
+      </div>
+      <!-- <el-col :span="8">
         <span :key="item.itemOrder"  v-for="item in answer.answerItems">
              <el-tag  class="do-exam-title-tag" >{{item.itemOrder}}</el-tag>
         </span>
         <span class="do-exam-time">
           <label>剩余时间：</label>
         </span>
-      </el-col>
-    </el-row>
-    <el-container  class="app-item-contain">
-      <el-header class="align-center">
-        <h1>{{form.name}}</h1>
-        <div>
-          <span class="question-title-padding">试卷总分：{{form.score}}</span>
-          <span class="question-title-padding">考试时间：{{form.suggestTime}}分钟</span>
-        </div>
-      </el-header>
+      </el-col> -->
+    <el-container class="my-do-exam-content">
       <el-main>
         <el-form :model="form" ref="form" v-loading="formLoading" label-width="100px">
           <el-row :key="index"  v-for="(titleItem,index) in form.titleItems">
-            <h3>{{titleItem.name}}</h3>
-            <el-card class="exampaper-item-box" v-if="titleItem.questionItems.length!==0">
+            <p class="my-do-exam-content-title">{{titleItem.name}}</p>
+            <div class="exampaper-item-box" v-if="titleItem.questionItems.length!==0">
               <el-form-item :key="questionItem.itemOrder" :label="questionItem.itemOrder+'.'"
                             v-for="questionItem in titleItem.questionItems"
                             class="exam-question-item" label-width="50px" :id="'question-'+ questionItem.itemOrder">
                 <QuestionEdit :qType="questionItem.questionType" :question="questionItem"
                               :answer="answer.answerItems[questionItem.itemOrder-1]"/>
+                <el-divider class="hr"></el-divider>
               </el-form-item>
-            </el-card>
+            </div>
           </el-row>
-           <el-row class="do-align-center">
-             <el-button type="primary" @click="submitForm">提交</el-button>
-             <el-button>取消</el-button>
-           </el-row>
+           <!-- <el-row class="do-align-center">
+             <el-button type="primary" @click="submitForm">提交答卷</el-button>
+           </el-row> -->
         </el-form>
       </el-main>
     </el-container>
@@ -172,5 +172,51 @@ export default {
   .question-title-padding {
     padding-left: 25px;
     padding-right: 25px;
+  }
+
+  .my-do-exam {
+    box-sizing: border-box !important;
+    background-color: rgb(239, 243, 247);
+    padding: 24px 80px;
+    position: relative;
+    &-info {
+      position: fixed;
+      background-color: white;
+      width: 268px;
+      height: 672px;
+      border: 1px solid #ebeef5;
+      border-radius: 4px;
+      padding: 20px;
+      &-sore {
+        font-size: 14px;
+      }
+      &-time {
+        font-size: 14px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    }
+    &-content {
+      // height: 662px;
+      margin-left: 300px;
+      border: 1px solid #ebeef5;
+      border-radius: 4px;
+      background-color: white;
+      &-title {
+        background: #fafafa;
+        width:100%;
+        height:60px;
+        padding-left: 30px;
+        line-height:60px;
+        font-size:18px;
+        border-top: 1px solid #dedede;
+        border-bottom: 1px solid #dedede;
+      }
+    }
+  }
+  .hr {
+    margin-left: -28px;
+    margin-bottom: 0px;
   }
 </style>
